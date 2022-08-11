20408fb785b3fdaba3e1eece76c45e7f6cd09a61
Frederik Baggesen <201804495@post.au.dk>
2022-08-11 17:48:21 +0200

    small fix

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         const MONGO_URI = "mongodb+srv://fredefrog123:fredefrog321@cluster0.ccmgbpe.mongodb.net/logical?retryWrites=true&w=majority"
         const conn = await mongoose.connect(MONGO_URI)
-        console.log("Succesfully connected to database!")
+        console.log("Succesfully connected to database! Hurray!")
     } catch(err) {
         console.log(err)
         process.exit(1)
